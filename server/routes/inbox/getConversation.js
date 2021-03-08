9e55add7194ad915e610992f0d12c545ecba56be
jeanpaul ngalula <jeanpaulngalula07@gmail.com>
2021-03-08 13:36:54 +0100

    add new conversation

--- server/routes/inbox/getConversation.js
+++ server/routes/inbox/getConversation.js
@@ -1,10 +1,21 @@
-import Match from '../../models/match';
+import Message from '../../models/Message';
 
 /**
-  * List All match of one user
+  * get a particular message
  */
 exports.getConversation = async (req, res, next) => {
   try {
+    const match_id = req.params.match_id ? req.params.match_id : '';
+    if (match_id === '') res.status(404).json({ msg: 'missed required params !' });
+
+    const conv = await Message.findOne({match_id: match_id}).catch((error) => { res.status(500).json({ error: error }); });
+    
+    if (!conv) {
+      return res.status(404).json({
+        error: new Error('User not found!')
+      });
+    }
+    res.status(200).json({ 'message': conv });
   } catch (error) {
     res.status(500).json({ msg: error });
   }
